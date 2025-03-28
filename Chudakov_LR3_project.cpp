4a35bc983550c3c0fff8ce72f56f7cffe5a82926
Stephanchik <krazyshadow79@gmail.com>
2025-03-28 09:48:35 +0300

    modified:   Chudakov_LR3_project.cpp 	modified:   Chudakov_LR3_project.exe

--- Chudakov_LR3_project.cpp
+++ Chudakov_LR3_project.cpp
@@ -7,6 +7,17 @@ int get_hour(int hour) {
     return (hour * 60);
 }
 
+int get_minute(int minute) {
+    return minute;
+}
+
+int hour_minute() {
+    return 1;
+}
+
+int diffirence() {
+    return 1;
+}
 int main() {
     setlocale(LC_ALL, "RUS");
     cout << "ok" << endl;
