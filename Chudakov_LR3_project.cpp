660bbb9ab89f270cbbbbe011e7f48adc54fd60ef
Stephanchik <krazyshadow79@gmail.com>
2025-03-28 09:23:14 +0300

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/tasks.json 	modified:   Chudakov_LR3_project.cpp 	new file:   Chudakov_LR3_project.exe

---
+++ Chudakov_LR3_project.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int get_hour(int hour) {
+    return (hour * 60);
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    cout << "ok" << endl;
+    return 0;
+}
